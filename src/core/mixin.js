3651e2bf0c4cfb9d45ba1fc2eff4bc25e7f69344
caixinquan <caixinquan@dfl.com.cn>
2021-02-10 10:32:43 +0800

    fix(core/mixin.js): 修复原型函数复制失败问题

--- src/core/mixin.js
+++ src/core/mixin.js
@@ -4,37 +4,36 @@ const tracePrototypeMethods = (obj) => {
   const methods = Object.getOwnPropertyNames(obj).reduce((acc, key) => {
     const descriptor = Object.getOwnPropertyDescriptor(obj, key);
     const { writable } = descriptor;
-    if (!writable) return acc;
+    if (!writable) {
+      return acc;
+    }
     const isEmpty = /^$/.test(key);
     const isReserve = ['constructor', 'mixins'].includes(key);
     const isHook = hooks.includes('key');
     if (!isEmpty && !isReserve && !isHook) {
-      return {
-        ...acc,
-        [key]: obj[key],
-      };
+      acc[key] = obj[key];
     }
     return acc;
   }, {});
   return methods;
 };
 
-const copyMethod = (instance) => {
+const copyMethod = (vm) => {
+  const instance = new vm();
   const { methods = {} } = instance;
-  const traceMethods = tracePrototypeMethods(instance.prototype);
-  return {
-    ...instance,
+  const traceMethods = tracePrototypeMethods(vm.prototype);
+  return Object.assign(instance, {
     methods: {
       ...methods,
       ...traceMethods,
     },
-  };
+  });
 };
 
 const createInstance = (vm) => {
   const isFunction = typeof vm === 'function';
   if (isFunction) {
-    return copyMethod(new vm());
+    return copyMethod(vm);
   }
   return Object.create(null);
 };
